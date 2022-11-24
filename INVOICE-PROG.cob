281dfeb117e781dd3eec994ed03d361bb7734e75
natanboanafina <boanafina.natan@gmail.com>
2022-11-24 02:20:19 -0300

    File completed

--- INVOICE-PROG.cob
+++ INVOICE-PROG.cob
@@ -4,7 +4,7 @@
       *    AUTHOR      : NATAN BOANAFINA.                              *
       *    ENTERPRISE  : NOT APPLICABLE                                *
       *    PROFESSOR   : IVAN PETRUCCI                                 *
-      *    DATE        : 23/11/2022                                    *
+      *    DATE        : 24/11/2022                                    *
       *                                                                *
       *----------------------------------------------------------------*
       *    PURPOSE     : SEARCH FOR CLIENTS VIA ID, SAVE THEIR GOODS   *
@@ -69,6 +69,7 @@
        01  RG-INVOICE.
            05 RG-INVOICE-NAME      PIC X(20).
            05 RG-INVOICE-PROD      PIC X(15).
+           05 RG-INVOICE-PRICE     PIC 9(08)V99.
 
       *----------------------------------------------------------------*
        WORKING-STORAGE                 SECTION.
@@ -89,54 +90,92 @@
       *----------------------------------------------------------------*
        77  WRK-CLID        PIC 9(05)  VALUE ZEROS.
        77  WRK-CTRL-VAR    PIC X(01)  VALUE SPACES.
-
-       01  WRK-SHOP.
-           05 WRK-SHOP-ID       PIC 9(05).
-           05 WRK-SHOP-ID-CLI   PIC 9(05).
-           05 WRK-SHOP-PROD     PIC X(15).
-           05 WRK-SHOP-PRICE    PIC 9(08)V99.
+       77  WRK-PRICE-ED    PIC ZZ.ZZZ.ZZZ,ZZ.
       *----------------------------------------------------------------*
        01  FILLER          PIC X(052) VALUE
            "============ WARNING MESSAGES BEGINNING ============".
       *----------------------------------------------------------------*
        77  WRK-OPEN-ERROR       PIC X(20) VALUE "FILE WAS NOT OPENED!".
        77  WRK-NOT-EMPTY        PIC X(18) VALUE "FILE IS NOT EMPTY!".
-      *================================================================*
       *----------------------------------------------------------------*
-      *================================================================*
        PROCEDURE                       DIVISION.
       *----------------------------------------------------------------*
            PERFORM 0100-INIT.
            PERFORM 0200-PROCESS.
            PERFORM 0300-END.
+           STOP RUN.
       *----------------------------------------------------------------*
        0100-INIT                       SECTION.
+      **================ OPENING-SHOPPING BEGINNING ==================**
        0110-OPEN-SHOPPING.
            OPEN INPUT SHOPPING.
                 IF FS-SHOPPING-STATUS NOT EQUAL 00
-                   DISPLAY WRK-OPEN-ERROR
-                   DISPLAY "STATUS: " FS-SHOPPING-STATUS
+                   DISPLAY "SHOPPING: " WRK-OPEN-ERROR
+                   DISPLAY "STATUS:   " FS-SHOPPING-STATUS
+                  GOBACK
+                END-IF.
+      **================ OPENING-CLIENTS BEGINNING ===================**
+       0120-OPEN-CLIENTS.
+           OPEN INPUT CLIENTS.
+                IF FS-CLIENTS-STATUS NOT EQUAL 00
+                   DISPLAY "CLIENTS: " WRK-OPEN-ERROR
+                   DISPLAY "STATUS:  " FS-CLIENTS-STATUS
+                  GOBACK
+                END-IF.
+      **================ OPENING-INVOICE BEGINNING ===================**
+       0130-OPEN-INVOICE.
+           OPEN OUTPUT INVOICE.
+                IF FS-INVOICE-STATUS NOT EQUAL 00
+                   DISPLAY "INVOICE: " WRK-OPEN-ERROR
+                   DISPLAY "STATUS:  " FS-INVOICE-STATUS
                   GOBACK
                 END-IF.
 
-      *----------------------------------------------------------------*
        0200-PROCESS                    SECTION.
+      **================ READING-CLIENTS BEGINNING ===================**
+       0210-READING-CLIENTS.
+           READ CLIENTS.
+             IF FS-CLIENTS-STATUS EQUAL 00
+                 PERFORM UNTIL FS-CLIENTS-STATUS NOT EQUAL 00
+                      DISPLAY "RG CLIENTES ID:   " RG-CLIENTS-ID
+                      DISPLAY "RG CLIENTES NOME: " RG-CLIENTS-NAME
+                   READ CLIENTS
+
+                 END-PERFORM
+                   MOVE RG-CLIENTS-NAME TO RG-INVOICE-NAME
+                   CLOSE CLIENTS
+             END-IF.
+      **================= READING SHOP BEGINING ======================**
        0220-READING-SHOPPING.
+           DISPLAY "DIGITE O ID DO CLIENTE: "
+             ACCEPT WRK-CLID.
            READ SHOPPING
              IF FS-SHOPPING-STATUS EQUAL 00
                 PERFORM UNTIL FS-SHOPPING-STATUS NOT EQUAL 00
-                 DISPLAY "ID DA COMPRA:      " RG-SHOPPING-ID
-                 DISPLAY "ID DO CLIENTE:     " RG-SHOPPING-ID-CLI
-                 DISPLAY "PRODUTO:           " RG-SHOPPING-PROD
-                 DISPLAY "PRECO:             " RG-SHOPPING-PRICE
-                 DISPLAY "------------------------------------------"
-                 READ SHOPPING
-
+                   IF WRK-CLID EQUAL RG-SHOPPING-ID
+                     DISPLAY "ID DA COMPRA:      " RG-SHOPPING-ID
+                     DISPLAY "ID DO CLIENTE:     " RG-SHOPPING-ID-CLI
+                     DISPLAY "PRODUTO:           " RG-SHOPPING-PROD
+                     DISPLAY "PRECO:             " RG-SHOPPING-PRICE
+                     DISPLAY "-----------------------------------------"
+                       MOVE RG-SHOPPING-PROD  TO RG-INVOICE-PROD
+                       MOVE RG-SHOPPING-PRICE TO WRK-PRICE-ED
+                       PERFORM 0230-WRITING-INVOICE
+                       CLOSE INVOICE
+                       CLOSE SHOPPING
+                   END-IF
+                   READ SHOPPING
                 END-PERFORM
-                CLOSE SHOPPING
                 GOBACK
              END-IF.
-
+      **================ WRITING-INVOICE BEGINING =====================*
+       0230-WRITING-INVOICE.
+           DISPLAY "MASK " WRK-PRICE-ED.
+           STRING RG-CLIENTS-NAME      DELIMITED BY SIZE
+                  RG-SHOPPING-PROD     DELIMITED BY SIZE
+                  RG-SHOPPING-PRICE    DELIMITED BY SIZE
+                  INTO RG-INVOICE.
+           WRITE RG-INVOICE.
       *----------------------------------------------------------------*
        0300-END                        SECTION.
            CLOSE CLIENTS.
