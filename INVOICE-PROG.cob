619aa987b84b8ad06660c4e5f18e5fe5f0186d5c
natanboanafina <boanafina.natan@gmail.com>
2022-11-24 00:12:30 -0300

    Reading Data: 0220-READING-SHOPPING ADDED

--- INVOICE-PROG.cob
+++ INVOICE-PROG.cob
@@ -80,37 +80,67 @@
        01  FILLER          PIC X(047) VALUE
            "============ FILE-STATUS BEGINNING ============".
       *----------------------------------------------------------------*
-       77  FS-CLIENTS-STATUS       PIC 9(02) VALUE ZEROS.
-       77  FS-SHOPPING-STATUS      PIC 9(02) VALUE ZEROS.
-       77  FS-INVOICE-STATUS       PIC 9(02) VALUE ZEROS.
+       77  FS-CLIENTS-STATUS       PIC 9(02).
+       77  FS-SHOPPING-STATUS      PIC 9(02).
+       77  FS-INVOICE-STATUS       PIC 9(02).
       *----------------------------------------------------------------*
-       01  FILLER          PIC X(050) VALUE
-           "============ ERROR MESSAGES BEGINNING ============".
+       01  FILLER          PIC X(057) VALUE
+           "============ PROCESSMENT VARIABLES BEGINNING ============".
+      *----------------------------------------------------------------*
+       77  WRK-CLID        PIC 9(05)  VALUE ZEROS.
+       77  WRK-CTRL-VAR    PIC X(01)  VALUE SPACES.
+
+       01  WRK-SHOP.
+           05 WRK-SHOP-ID       PIC 9(05).
+           05 WRK-SHOP-ID-CLI   PIC 9(05).
+           05 WRK-SHOP-PROD     PIC X(15).
+           05 WRK-SHOP-PRICE    PIC 9(08)V99.
+      *----------------------------------------------------------------*
+       01  FILLER          PIC X(052) VALUE
+           "============ WARNING MESSAGES BEGINNING ============".
       *----------------------------------------------------------------*
        77  WRK-OPEN-ERROR       PIC X(20) VALUE "FILE WAS NOT OPENED!".
+       77  WRK-NOT-EMPTY        PIC X(18) VALUE "FILE IS NOT EMPTY!".
       *================================================================*
       *----------------------------------------------------------------*
       *================================================================*
        PROCEDURE                       DIVISION.
       *----------------------------------------------------------------*
            PERFORM 0100-INIT.
-      *     PERFORM 0110-ADD-DATA UNTIL CTRL-VAR NOT EQUAL "N".
            PERFORM 0200-PROCESS.
            PERFORM 0300-END.
       *----------------------------------------------------------------*
        0100-INIT                       SECTION.
+       0110-OPEN-SHOPPING.
            OPEN INPUT SHOPPING.
                 IF FS-SHOPPING-STATUS NOT EQUAL 00
                    DISPLAY WRK-OPEN-ERROR
                    DISPLAY "STATUS: " FS-SHOPPING-STATUS
                   GOBACK
                 END-IF.
-      *----------------------------------------------------------------*
-       0200-PROCESS                    SECTION.
-           CONTINUE.
-      *----------------------------------------------------------------*
-       0300-END                        SECTION.
-           CLOSE SHOPPING.
 
       *----------------------------------------------------------------*
-      *================================================================*
+       0200-PROCESS                    SECTION.
+       0220-READING-SHOPPING.
+           READ SHOPPING
+             IF FS-SHOPPING-STATUS EQUAL 00
+                PERFORM UNTIL FS-SHOPPING-STATUS NOT EQUAL 00
+                 DISPLAY "ID DA COMPRA:      " RG-SHOPPING-ID
+                 DISPLAY "ID DO CLIENTE:     " RG-SHOPPING-ID-CLI
+                 DISPLAY "PRODUTO:           " RG-SHOPPING-PROD
+                 DISPLAY "PRECO:             " RG-SHOPPING-PRICE
+                 DISPLAY "------------------------------------------"
+                 READ SHOPPING
+
+                END-PERFORM
+                CLOSE SHOPPING
+                GOBACK
+             END-IF.
+
+      *----------------------------------------------------------------*
+       0300-END                        SECTION.
+           CLOSE CLIENTS.
+           CLOSE SHOPPING.
+           CLOSE INVOICE.
+
+      *----------------------------------------------------------------*
